385e8772f1a4766d9199415318edb14e75a5f7f3
Laplace <johnalexisbuot@gmail.com>
2020-08-14 02:39:24 +0800

    Added color change for the dark mode text

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleCircle = document.querySelector(".heading__toggle--circle");
 
     let toggled = false;
-
+    
     toggle.addEventListener("click", switchThemeHandler);
 
     function switchThemeHandler() {
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
             document.documentElement.style.setProperty(`--bg-primary-color`, `hsl(0, 0%, 100%)`);
             document.documentElement.style.setProperty(`--bg-secondary-color`, `hsl(225, 100%, 98%)`);
             document.documentElement.style.setProperty(`--primary-color`, `hsl(227, 47%, 96%)`);
-            document.documentElement.style.setProperty(`--secondary-color`, `hsl(228, 12%, 44%)`);
-            document.documentElement.style.setProperty(`--accent-color`, `hsl(230, 17%, 14%)`);
+            document.documentElement.style.setProperty(`--secondary-color`, `hsl(230, 17%, 14%)`);
+            document.documentElement.style.setProperty(`--accent-color`, `hsl(228, 12%, 44%)`);
             toggle.classList.add("active");
             toggleCircle.classList.add("is-toggled");
         } else {
